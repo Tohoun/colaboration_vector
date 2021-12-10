e7d8be6cd6af49298257b4ada101e79f8a152249
Romeo tohoun <romeotohoun@gmail.com>
2021-12-10 16:15:19 +0000

    Ajout de la methode qui verifie si deux vecteurs sont colineaires

--- vector.py
+++ vector.py
@@ -18,19 +18,30 @@ class Vector2D:
     def y(self,new_y):
         self.__y = new_y
 
+    # Methode pour calculer la norme
     def norme(self):
         return sqrt(self.__x**2+self.__y**2) 
 
+    # Methode pour faire l'adition
     def addition(self,t):
         self.__x += t.x
         self.__y += t.y 
 
+    # Methode pour faire une soustraction
     def soustraction(self,t):
         self.__x -= t.x
         self.__y -= t.y
 
+    # Methode pour faire le produit scalaire
     def produit_scalaire(self,t):
         return self.__x * t.x + self.__y * t.y 
+
+    # Methode pour verifier la colinearité entre deux vecteurs
+    def colinearite(self,t):
+        if((self.__x*t.y-self.y*t.x)==0):
+            print("ils sont colinéaires")
+        else:
+            print("Ils sont pas colinéaires")
 
 
 v1 = Vector2D(2,5)
@@ -43,3 +54,5 @@ print(v1.x, v1.y, sep ='\n')
 
 v1.soustraction(v2)
 print(v1.x, v1.y, sep ='\n')
+
+v1.colinearite(v2)
